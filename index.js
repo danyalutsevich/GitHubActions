c138a9de1f2850559b25b53311785b261aadd1c8
danyalutsevich <luchevich31@gmail.com>
2023-04-26 20:26:20 +0300

    bye world

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.get('/hey', (req, res) => {
+    res.send('Bye World!');
+})
+
 console.log('Server is running on port 3333');
 app.listen(3333)
